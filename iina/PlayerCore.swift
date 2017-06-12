7537c8faab2798a33613561db1a3b28dbcee6753
Collider LI <lhc199652@gmail.com>
2017-06-13 04:05:07 +0800

    improve accuracy

--- iina/PlayerCore.swift
+++ iina/PlayerCore.swift
@@ -1143,7 +1143,7 @@ class PlayerCore: NSObject {
 
 
 extension PlayerCore: FFmpegControllerDelegate {
-  func didGeneratedThumbnails(_ thumbnails: [NSImage]!, withSuccess success: Bool) {
+  func didGeneratedThumbnails(_ thumbnails: [FFThumbnail]!, withSuccess success: Bool) {
     if success {
       info.thumbnailsReady = true
       info.thumbnails = thumbnails
